c63567766845470b23cd16f0ff8f4862a47757f3
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-05-03 23:43:01 +0200

    Añadida implememtación en Python

---
+++ Python/Collatz.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+
+
+# Función para implementar el bucle principa
+#
+# @param num
+#           Número cuya serie va a ser impresa
+#
+def collatz (num):
+
+    paso = 0
+    while num > 1:
+        if (num % 2) == 0:
+            num /= 2
+        else:
+            num *= 3
+            num += 1
+
+        paso += 1
+	print "Paso %5i: %10li" % (paso, num)
+
+    return
+
+# Comprueba los argumentos
+if len (sys.argv) != 2:
+    print "Error -> Llamada correcta: \n\t%s <número_inicial>\n" % sys.argv [0]
+    exit (-1)
+
+# Obtiene el número e inicia la serie
+num = long (sys.argv [1])
+
+print "\nNúmero inicial: %li" % num
+collatz (num)
+print "---- FIN DE LA SERIE ----"
--- README.md
+++ README.md
@@ -12,3 +12,4 @@
   - Bash
   - Lisp
   - Prolog
+  - Python
