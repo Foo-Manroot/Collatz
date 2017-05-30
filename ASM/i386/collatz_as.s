3858d719090460394494c2a7d6e21ab90cb381d9
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-05-30 19:04:51 +0200

    Terminada la implementación en ensamblador (para 32 bit)

--- ASM/i386/collatz_as.s
+++ ASM/i386/collatz_as.s
@@ -7,15 +7,10 @@
 		.asciz "Error -> Llamada correcta: \n\t%s <número_inicial>\n"
 	form_inicio:
 		.asciz "\nNúmero inicial: %i\n"
+	form_paso:
+		.asciz "\nPaso %5i: %10li"
 	msg_fin:
 		.asciz "\n---- FIN DE LA SERIE ----\n"
-
-
-	form_hex:
-		.asciz "Hexadecimal: %x\nEntero: %i\n"
-
-	form_debug:
-		.asciz "DEBUG: %i\n"
 
 .section .text
 .globl _start
@@ -40,11 +35,8 @@ _start:
 
 # ebp == 2
 cont:
-	# Obtiene el puntero a argv [1]
-	movl	8(%ebp), %eax
-
 	# Obtiene en EAX el valor numérico de argv [1]
-	pushl	%eax
+	pushl	8(%ebp)
 	call	obtener_arg
 	add	$4, %esp
 
@@ -55,6 +47,10 @@ cont:
 	add	$4, %esp
 	popl	%eax
 
+	# Llama a la función para imprimir la serie
+	pushl	%eax
+	call	collatz
+	add	$4, %esp
 
 	# printf ($msg_fin)
 	pushl	$msg_fin
@@ -66,9 +62,95 @@ cont:
 
 
 ####
+# FUNCIONES AUXILIARES
+####
+
+
+####
+# Función para imprimir los números de la serie
+#
+# Args (por pila):
+#	-> El número inicial de la serie
+####
+collatz:
+	pushl	%ebp
+	# Guarda los registros usados: EAX, ECX, EDX
+	pushl	%eax
+	pushl	%ecx
+	pushl	%edx
+
+	# Obtiene el argumento y lo carga en EAX
+	movl	20(%esp), %eax
+
+	xor	%ecx, %ecx
+
+	# Primero imprime el paso 0 y luego sigue con el bucle (do-while)
+	jmp collatz_bucle_imprimir
+
+	# Fin del bucle -> EAX = 1
+	#
+	# Se usan los siguientes registros:
+	#	-> EAX para almacenar el valor actual del número
+	#	-> ECX como contador de pasos
+	#	-> EDX como registro auxiliar para las operaciones
+	collatz_bucle:
+		cmpl	$1, %eax
+		jle	collatz_fin
+
+		inc	%ecx
+		# Comprueba si EAX es par (si el LSB es 0)
+		movl	$1, %edx
+		and	%eax, %edx
+		jz	collatz_bucle_elem_par	# Si ZF = 1 -> el bit está a 0 -> es par
+
+		# Es impar: EAX <- (3 * EAX) + 1
+		pushl	%ecx		# Guarda ECX
+		movl	$3, %ecx
+		mul	%ecx		# EDX:EAX <- EAX * 3 (EDX se ignora)
+		popl	%ecx		# Recupera ECX
+
+		inc	%eax
+
+		jmp collatz_bucle_imprimir
+		collatz_bucle_elem_par:
+
+			pushl	%ecx		# Guarda ECX
+			movl	$2, %ecx
+			div	%ecx		# EAX <- EDX:EAX / 2 (en ECX), EDX <- 0
+			popl	%ecx		# Recupera ECX
+
+		collatz_bucle_imprimir:
+			# Guarda EAX y ECX
+			pushl	%eax
+			pushl	%ecx
+
+			# printf ($form_paso, paso, num)
+			pushl	%eax
+			pushl	%ecx
+			pushl	$form_paso
+			call	printf
+			add	$12, %esp
+
+			# Restaura EAX y ECX
+			popl	%ecx
+			popl	%eax
+
+			jmp collatz_bucle
+
+	collatz_fin:
+		# Restaura los registros usados: EAX, ECX, EDX
+		popl	%edx
+		popl	%ecx
+		popl	%eax
+
+		popl	%ebp
+		ret
+
+
+####
 # Función para obtener el valor numérico de la cadena pasada en argv [1] (el número
 # inicial de la serie).
-# Para ello, comprueba Bytes hasta que encuentra 0x00
+# Para ello, comprueba bytes hasta que encuentra 0x00.
 #
 # Args (por pila):
 #	-> La dirección de memoria del argumento a comprobar (el valor de X(%ebp) )
@@ -115,9 +197,9 @@ obtener_arg:
 
 		# Multiplica lo acumulado por 10 y le suma lo nuevo
 		pushl	%edx
-		mul	%ecx		# EDX:EAX = EAX * 10
-
+		mul	%ecx		# EDX:EAX <- EAX * 10
 		popl	%edx
+
 		add	%edx, %eax	# EDX <- EDX + EAX
 
 		inc	%ebx
